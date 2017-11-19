9be5a39a782ef3d16bc1a6517dd039a3c23c5ca7
slimtimtam <ltara8@vt.edu>
2017-11-19 11:22:25 -0500

    implemented handle class
    getters, setters, and one random thing we might need idk yet

--- src/Handle.java
+++ src/Handle.java
@@ -3,10 +3,74 @@
  */
 
 /**
+ * Stores values for finding data in the array
  * @author Purnima Ghosh
+ * @author ltara8
  * @version Nov 16, 2017
  *
  */
-public class Handle {
+public class Handle 
+{
+    private int off;
+    private int len;
+    /**
+     * constructor for handle class
+     * @param offset locations in the array of the data
+     * @param length length of the data
+     */
+    public Handle(int offset, int length)
+    {
+        this.off = offset;
+        this.len = length;
+    }
+    /**
+     * gets the offset
+     * @return int offset
+     */
+    public int getOff() 
+    {
+        return off;
+    }
+    /**
+     * sets the offset
+     * @param off the offset given
+     */
+    public void setOff(int off) 
+    {
+        this.off = off;
+    }
+    /**
+     * gets length of offset
+     * @return int offset
+     */
+    public int getLen() 
+    {
+        return len;
+    }
+    /**
+     * sets the length of offset
+     * @param len length
+     */
+    public void setLen(int len) 
+    {
+        this.len = len;
+    }
+    /**
+     * compares if two handles are equal
+     * @param hand hangle to be compared
+     * @return boolean true or false
+     */
+    public boolean equals(Handle hand)
+    {
+        return off == hand.getOff() && len == hand.getLen();
+    }
+    
+    
+    // I DONT KNOW IF WE NEED THIS YET BUT WE CAN KEEP IT FOR NOW
+    public int byteCalculation()
+    {
+        return (len + 3) / 4;   
+    }
+    
 
 }
