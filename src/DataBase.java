ee4f8d2d00e1e96c839d48e646f7e5432bf40afa
slimtimtam <ltara8@vt.edu>
2017-11-20 23:03:29 -0500

    dataBase updates
    worked on insert for database but it isnt done

--- src/DataBase.java
+++ src/DataBase.java
@@ -1,20 +1,26 @@
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Scanner;
 
 public class DataBase 
 {
     Scanner fileScanner;
-    byte[] massiveByteArr;
+    //byte[] massiveByteArr;
+    ArrayList<Byte> massiveByteArr;
     int resize = 0;
     
-    HashTable hash;
+    HashTable hashArtist;
+    HashTable hashSong;
     //this class is gonna take care of array stuff like making it and all dat
     public DataBase()
     {
-        massiveByteArr = new byte[resize];
-        hash = new HashTable();
+        //massiveByteArr = new byte[resize];
+        massiveByteArr = new ArrayList<>();
+        hashArtist = new HashTable();
+        hashSong = new HashTable();
     }
     
     public void readFile(String fileName) throws FileNotFoundException
@@ -29,20 +35,56 @@ public class DataBase
             String instruction = fileScanner.next();
             if (instruction.equals("insert"))
             {
-                resize += 3;
-                massiveByteArr = Arrays.copyOf(massiveByteArr, resize);
-                for (int i = 0; i < massiveByteArr.length; i++)
-                {
-                    
-                }
+                
             }
 
             
         }
         
     }
-    public void makeByteArray()
+    public void insert()
     {
+        StringBuilder artist = new StringBuilder();
+        StringBuilder songTitle = new StringBuilder();
+        //creates a string so we can read in the artist
+        while (fileScanner.next() != "<SEP>")
+        {
+           artist.append(fileScanner.next());
+        }
+        fileScanner.skip("<SEP>");
+        while (fileScanner.hasNext())
+        {
+           songTitle.append(fileScanner.next()); 
+        }
+        //these strings can be turned into bytes which can be
+        String artistString = artist.toString();
+        String songTitleString = songTitle.toString();
+        //inserted into our byte array
+        byte[] songBytes = songTitleString.getBytes();
+        byte[] artistBytes = artistString.getBytes();
+        
+        int lengthOfArtist = artistBytes.length;
+        int lengthOfSong = songBytes.length;
+        
+//        resize += (3 + lengthOfArtist + lengthOfSong);
+//        massiveByteArr = Arrays.copyOf(massiveByteArr, massiveByteArr.length + resize);
+//        for (int i = 0; i < resize; i++)
+//        {
+//            
+//        }
+        int s = 1;
+        massiveByteArr.add((byte) s);
+        
+        massiveByteArr.add((byte) (lengthOfArtist + lengthOfSong));
+        for (int k = 0; k < artistBytes.length; k++)
+        {
+            massiveByteArr.add(artistBytes[k]);
+        }
+        for (int k = 0; k < songBytes.length; k++)
+        {
+            massiveByteArr.add(songBytes[k]);
+        }
+        Handle hand = new Handle(massiveByteArr.indexOf(s), (lengthOfArtist + lengthOfSong));
         
     }
 }
