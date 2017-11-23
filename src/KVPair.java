7190dd4b432ff984044ab68a52feb76fa042f366
purnima <purnima@vt.edu>
2017-11-23 13:40:16 -0500

    Implemented KVPair compareTo method

--- src/KVPair.java
+++ src/KVPair.java
@@ -11,7 +11,7 @@ public class KVPair implements Comparable<KVPair>
 {
     private Handle key;
     private Handle value;
-    
+
     public KVPair(Handle k, Handle v)
     {
         key = k;
@@ -19,13 +19,35 @@ public class KVPair implements Comparable<KVPair>
     }
 
     /**
-     * @param o
-     * @return
+     * This method compares two KVPair objects and returns
+     * the proper ordering
+     * @param object the object being compared
+     * @return an int that represents the correct ordering
      */
     @Override
-    public int compareTo(KVPair o) {
-        // TODO Auto-generated method stub
-        return 0;
+    public int compareTo(KVPair object) 
+    {
+        if (this.key.getOff() < object.key.getOff())
+        {
+            return -1;
+        }
+        else if (this.key.getOff() > object.key.getOff())
+        {
+            return 1;
+        }
+        else if (this.value.getOff() < object.value.getOff())
+        {
+            return -1;
+        }
+        else if (this.value.getOff() > object.value.getOff())
+        {
+            return 1;
+        }
+        //might be unnecessary because we can't have duplicates right?
+        else
+        {
+            return 0;
+        }
     }
 
 }
