ff6aa23329e9e20a04ddeddfadc23ad33c6132d1
purnima <purnima@vt.edu>
2017-12-03 22:13:44 -0500

    Added tests for memory manager test

--- src/MemoryManagerTest.java
+++ src/MemoryManagerTest.java
@@ -40,7 +40,59 @@ public class MemoryManagerTest extends student.TestCase
         Handle[] test2 = testMM.add(artist, "otherSong");
         ArrayList<Handle> arr = testMM.findHandle();
         assertTrue(arr.contains(test1[0]));
+        assertTrue(arr.contains(test2[1]));
         assertFalse(arr.contains(test2[0]));
     }
-    
+    /**
+     * Test add after a handle has been removed
+     */
+    public void testAddRemoveArtist()
+    {
+        testMM.add(artist, song);
+        testMM.remove(artist);
+        Handle[] test = testMM.add(artist, song);
+        ArrayList<Handle> arr = testMM.findHandle();
+        assertTrue(arr.contains(test[1]));
+    }
+    /**
+     * Test add after a handle has been removed
+     */
+    public void testAddRemoveSong()
+    {
+        testMM.add(artist, song);
+        testMM.remove(song);
+        Handle[] test = testMM.add(artist, song);
+        ArrayList<Handle> arr = testMM.findHandle();
+        assertTrue(arr.contains(test[1]));
+    }
+    /**
+     * Tests removing an artist from the database
+     */
+    public void testRemoveArtist()
+    {
+        testMM.add(artist, song);
+        boolean check = testMM.remove(artist);
+        ArrayList<Handle> arr = testMM.findHandle();
+        assertTrue(check);
+        assertTrue(arr.isEmpty());
+    }    
+    /**
+     * Tests removing an song from the database
+     */
+    public void testRemoveSong()
+    {
+        testMM.add(artist, song);
+        boolean check = testMM.remove(song);
+        ArrayList<Handle> arr = testMM.findHandle();
+        assertTrue(check);
+        assertTrue(arr.isEmpty());
+    }
+    /**
+     * Tests remove when there is nothing in the database
+     */
+    public void testRemoveEmpty()
+    {
+        boolean check = testMM.remove(artist);
+        assertFalse(check);
+    }
 }
